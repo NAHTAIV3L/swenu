eb62e6d21d6492d6e7d4894bd0298ad85c0b8a0a
Riley Beckett <rbeckettvt@gmail.com>
2025-10-18 13:30:02 -0400

    make swenu-run.sh faster

--- swenu-run.sh
+++ swenu-run.sh
@@ -3,7 +3,5 @@
 IFS=:
 exec $(
 	for dir in $PATH; do
-		for file in "$dir"/*; do
-			[ -f "$file" ] && [ -x "$file" ] && printf '%s\n' "${file##*/}"
-		done
-	done | sort -u | ./swenu -cal 10)
+		[ -e ${dir} ]  && find ${dir} -executable -printf '%f\n'
+	done | sort -u | ./swenu -cal)

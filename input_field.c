677a18dbd874debbdc2bf119d804dfd99563719a
Jack Jamison <jackqjamison@gmail.com>
2025-10-19 23:04:18 -0400

    quick qol

--- input_field.c
+++ input_field.c
@@ -24,7 +24,7 @@ void filter_items(client_state* state) {
 			array_add(state->filtered_items, (item_display_t){ .item = i });
 		}
 	}
-	qsort(state->filtered_items, array_size(state->filtered_items), sizeof(item_display_t), compare);
+	if (array_size(state->input_buffer)) qsort(state->filtered_items, array_size(state->filtered_items), sizeof(item_display_t), compare);
 
 	// select item
 	if (array_size(state->filtered_items) > 0) state->selected_filtered_item = 0;
